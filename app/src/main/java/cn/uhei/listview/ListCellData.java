6ef95d528283a01d20ea7f8a2ba3fec11613f60f
Administrator <cmba@qq.com>
2016-01-18 09:48:36 +0800

    最简单的listView使用方法

---
+++ app/src/main/java/cn/uhei/listview/ListCellData.java
@@ -0,0 +1,7 @@
+package cn.uhei.listview;
+
+/**
+ * 自定义列表数据模型.
+ */
+public class ListCellData {
+}
---
+++ app/src/main/java/cn/uhei/listview/MainActivity.java
@@ -0,0 +1,33 @@
+package cn.uhei.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    //2字符串类型的adapter
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //3初始化adapter
+        //arg 上下文，每一行的布局样式(可以使用系统自带的)，
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+
+        //添加数据
+        adapter.add("niupeiwang ");
+        adapter.add("Hello");
+        adapter.add("android");
+
+        listView = (ListView) findViewById(R.id.listView);
+        //1列表需要adapter来填充值
+        listView.setAdapter(adapter);
+
+    }
+}
